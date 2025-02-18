5a5f79ceab53c96c75722c735bfddc3562061df2
Luis Carlos <133696936+LucaolDev@users.noreply.github.com>
2025-02-17 22:34:25 -0300

    Add files via upload

---
+++ Programação Orientada a Objetos com Java e WEB/src/Exercicio01/Ex01.java
@@ -0,0 +1,24 @@
+package Exercicio01;
+import java.util.Scanner;
+
+public class Ex01 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);    
+        Aluno aluno1 = new Aluno(null, null, null, null);
+        
+        System.out.println("Digite o RM do aluno: ");
+        aluno1.setRm(in.nextInt());
+        System.out.println("Digite o Nome do aluno");
+        in.nextLine();
+        aluno1.setNome(in.nextLine());
+        System.out.println("Digite a primeira nota do aluno");
+        aluno1.setNota1(in.nextDouble());
+        System.out.println("Digite a segunda do aluno");
+        aluno1.setNota2(in.nextDouble());
+        System.out.println(aluno1);
+
+        in.close();
+    }
+}
