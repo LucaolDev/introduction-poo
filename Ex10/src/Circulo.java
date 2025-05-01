817f9878a5e97eedddb4d0621b798909eb9ba333
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-30 22:20:26 -0300

    commit

---
+++ Ex10/src/Circulo.java
@@ -0,0 +1,12 @@
+public class Circulo extends Forma{
+
+    public Circulo(int cordenadaX, int cordenadaY, Double raio) {
+        super(cordenadaX, cordenadaY, raio);
+    }
+
+    public double calcularArea(){
+        return Math.PI * Math.pow(getRaio(), 2);
+    }
+
+
+}
